02fafc6bd833d38f0beab5b056330ba373a0c221
ads04r <ads04r@408bdfbd-0bd4-0410-88bf-c0b946b28f18>
2013-03-06 16:54:12 +0000

    Added first version of the MOTD script/class. Very simple atm, just returns colloquial comment based on the weather (how english!)
    git-svn-id: svn+ssh://svn.forge.ecs.soton.ac.uk/projects/ecsrhino/opendata@6 408bdfbd-0bd4-0410-88bf-c0b946b28f18

---
+++ motd.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+from mechanize import Browser
+import re
+
+class rhinoMotd:
+	def message(self):
+		return self.weatherMessage()
+	def weatherMessage(self):
+		url = 'http://open.live.bbc.co.uk/weather/feeds/en/2637487/observations.rss'
+		br = Browser()
+		br.set_handle_robots(False)
+		page = br.open(url)
+		xml = page.read()
+		itemr = re.compile('<item>(.*?)</item>', re.DOTALL)
+		descr = re.compile('<description>(.*?)</description>', re.DOTALL)
+		tempr = re.compile('([0-9]+)..C')
+		tempintr = re.compile('[0-9]+')
+		item = str(itemr.search(xml).group(0))
+		desc = str(descr.search(item).group(0))
+		temp = str(tempr.search(desc).group(0))
+		tempint = int(tempintr.search(temp).group(0))
+		if tempint<14:
+			rv = "Brr! It's a bit cold for rhinos in Southampton! We're used to far higher temperatures in southern Africa!"
+		elif tempint<20:
+			rv = "I'm quite liking the weather today, it's like a cool winter's day in southern Africa."
+		elif tempint<=26:
+			rv = "I'm quite liking the weather today, it's nice and warm, just like it is back home!"
+		else:
+			rv = "Ooh, it's hot today! My thick rhino skin can't take much more!"
+		return rv
+
+if __name__ == '__main__':
+	rhino = rhinoMotd()
+	msg = rhino.message()
+	print msg
